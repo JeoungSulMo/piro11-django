82199ea0c10a510f342557ffcf813da8c3148016
정성모 <tjdah0853@gmail.com>
2019-07-17 16:13:10 +0900

    첫앱, urlconf, 뷰, 템플릿

--- startproject1/blog/models.py
+++ startproject1/blog/models.py
@@ -1,10 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class Category(models.Model):
-    name = models.CharField(max_length=100)
-
-
-class Post(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    message = models.CharField(max_length=100)
