7ccc385ba345b070df19ea5c0fe318f2e50f39e8
정성모 <tjdah0853@gmail.com>
2019-07-19 16:29:58 +0900

    blog.admin.Post 모델에 status 필드를 추가하고, 이를 처리하는 Custom Admin Action 구현

--- startproject1/blog/admin.py
+++ startproject1/blog/admin.py
@@ -3,7 +3,8 @@ from .models import Post
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title','content_size', 'created_at', 'updated_at']
+    list_display = ['id', 'title', 'status', 'content_size', 'created_at', 'updated_at']
+    actions = ['make_published', 'make_Draft', 'make_withdrawn']
 
     def content_size(self, post):
         return '{}글자' .format(len(post.content))
@@ -12,3 +13,19 @@ class PostAdmin(admin.ModelAdmin):
     content_size.allow_tags = True
 #admin.site.register(Post, PostAdmin)
 
+    def make_withdrawn(self, request, queryset):
+        updated_count = queryset.update(status='w')
+        self.message_user(request, '{}건의 포스팅을 withdrawn 상태로 변경'.format(updated_count))
+    make_withdrawn.short_description = '지정포스팅을 withdrawn 상태로 변경합니다.'
+
+
+    def make_Draft(self, request, queryset):
+        updated_count = queryset.update(status='d')
+        self.message_user(request, '{}건의 포스팅을 Draft 상태로 변경'.format(updated_count))
+    make_Draft.short_description = '지정포스팅을 Draft 상태로 변경합니다.'
+
+
+    def make_published(self, request, queryset):
+        updated_count = queryset.update(status='p')
+        self.message_user(request, '{}건의 포스팅을 published 상태로 변경'.format(updated_count))
+    make_published.short_description = '지정포스팅을 published 상태로 변경합니다.'
--- startproject1/blog/models.py
+++ startproject1/blog/models.py
@@ -9,14 +9,20 @@ def lnglat_validator(value):
         raise ValidationError('Invalid LngLat Type')
 
 
+
 # Create your models here.
 
 class Post(models.Model):
+    STATUS_CHOICES = (
+        ('d', 'Draft'),
+        ('p', 'Published'),
+        ('w', 'Withdrawn'),
+    )
     author = models.CharField(max_length=20)
     title = models.CharField(max_length=100, verbose_name='제목', help_text='포스팅 제목을 입력해주세요 최대 100자 안으로'
                              # choices=(
                              #     ('제목1', '제목1 레이블'),
-                             #     ('제목2', '제목2 레이블'),py
+                             #     ('제목2', '제목2 레이블'),
                              #     ('제목3', '제목3 레이블'),
                              )
     content = models.TextField(verbose_name='내용')
@@ -24,6 +30,7 @@ class Post(models.Model):
     lnglat = models.CharField(max_length=50, blank=True,
                               validators=[lnglat_validator],
                               help_text='경도, 위도 포멧으로 입력')
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
