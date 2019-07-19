25085580fff859cb6cf6df1b6bac4c25c29300df
정성모 <tjdah0853@gmail.com>
2019-07-19 15:13:21 +0900

    blog.admin.PostAdmin에 list_distplay 커스텀

--- startproject1/blog/admin.py
+++ startproject1/blog/admin.py
@@ -3,6 +3,12 @@ from .models import Post
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'created_at', 'updated_at']
+    list_display = ['id', 'title','content_size', 'created_at', 'updated_at']
 
+    def content_size(self, post):
+        return '{}글자' .format(len(post.content))
+
+    content_size.short_description = '내용 글자수'
+    content_size.allow_tags = True
 #admin.site.register(Post, PostAdmin)
+
