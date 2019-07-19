fd99b833b4d9470ee9e29075da5ea6cc86f6250b
정성모 <tjdah0853@gmail.com>
2019-07-19 11:24:15 +0900

    블로그앱 모델설명

--- startproject1/blog/models.py
+++ startproject1/blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100, verbose_name='제목', help_text='포스팅 제목을 입력해주세요 최대 100자 안으로',
+                             choices=(
+                                 ('제목1', '제목1 레이블'),
+                                 ('제목2', '제목2 레이블'),
+                                 ('제목3', '제목3 레이블'),
+                             ))
+    content = models.TextField(verbose_name='내용')
+    tag = models.CharField(max_length=100, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
--- startproject1/dojo/urls.py
+++ startproject1/dojo/urls.py
@@ -21,12 +21,12 @@ from . import views_cbv
 #     path('excel/', views.excel_download)
 # }
 
-urlpatterns = {
+urlpatterns = [
     path('cbv/list1/', views_cbv.post_list1),
     path('list3/', views.post_list3),
     path('cbv/list2/', views_cbv.post_list2),
     # path('cbv/list2/', views_cbv.post_list2),
     # path('cbv/list3/', views_cbv.post_list3),
     # path('cbv/excel/', views_cbv.excel_download),
-}
+]
 
