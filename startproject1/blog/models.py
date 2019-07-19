1ed00120d2963c1c35f8a4e4582a4ee165ae7a99
정성모 <tjdah0853@gmail.com>
2019-07-19 15:00:47 +0900

    blog.models.Post 모델에 대한 PostAdmin 커스텀

--- startproject1/blog/models.py
+++ startproject1/blog/models.py
@@ -12,12 +12,13 @@ def lnglat_validator(value):
 # Create your models here.
 
 class Post(models.Model):
-    title = models.CharField(max_length=100, verbose_name='제목', help_text='포스팅 제목을 입력해주세요 최대 100자 안으로',
-                             choices=(
-                                 ('제목1', '제목1 레이블'),
-                                 ('제목2', '제목2 레이블'),
-                                 ('제목3', '제목3 레이블'),
-                             ))
+    author = models.CharField(max_length=20)
+    title = models.CharField(max_length=100, verbose_name='제목', help_text='포스팅 제목을 입력해주세요 최대 100자 안으로'
+                             # choices=(
+                             #     ('제목1', '제목1 레이블'),
+                             #     ('제목2', '제목2 레이블'),py
+                             #     ('제목3', '제목3 레이블'),
+                             )
     content = models.TextField(verbose_name='내용')
     tag = models.CharField(max_length=100, blank=True)
     lnglat = models.CharField(max_length=50, blank=True,
