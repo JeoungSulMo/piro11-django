1f1392916a73ef388bc586d25a174b29254daafb
정성모 <tjdah0853@gmail.com>
2019-07-21 16:07:47 +0900

    get_object_or_404 추가

--- startproject1/blog/views.py
+++ startproject1/blog/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
 from .models import Post
+
+
 # Create your views here.
 
 def post_list(request):
@@ -9,6 +12,14 @@ def post_list(request):
     if q:
         qs = qs.filter(title__icontains=q)
 
-    return render(request, 'blog/post_list.html', {'post_list': qs, 'q': q,}, )
+    return render(request, 'blog/post_list.html', {'post_list': qs, 'q': q})
 
 
+def post_detail(request, id):
+    # try:
+    #     post = Post.objects.get(id=id)
+    # except Post.DoesNotExist:
+    #     raise Http404
+    #post = Post.objects.get(id=id)
+    post = get_object_or_404(Post, id=id)
+    return render(request, 'blog/post_detail.html', {'post': post})
