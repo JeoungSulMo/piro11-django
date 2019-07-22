e618da784c2ad30cb66b987ab4a1c799564912d2
정성모 <tjdah0853@gmail.com>
2019-07-22 13:15:21 +0900

    accounts.models.Profile model 잘못올려서 다시수정

--- startproject1/accounts/models.py
+++ startproject1/accounts/models.py
@@ -3,6 +3,6 @@ from django.db import models
 
 # Create your models here.
 class Profile(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone_number = models.CharField(max_length=20)
     address = models.CharField(max_length=50)
