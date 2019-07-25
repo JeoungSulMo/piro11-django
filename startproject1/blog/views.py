a4dba64510573a655c51cd966981e92fd35f61e8
정성모 <tjdah0853@gmail.com>
2019-07-25 17:50:10 +0900

    dojo.models.Post 모델을 추가하고, 마이그래이션

--- startproject1/blog/views.py
+++ startproject1/blog/views.py
@@ -1,5 +1,5 @@
 from django.http import Http404
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
 
 
@@ -23,3 +23,13 @@ def post_detail(request, id):
     #post = Post.objects.get(id=id)
     post = get_object_or_404(Post, id=id)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+
+def create(request):
+    if request.POST:
+        new_post = Post()
+        new_post.user = request.GET['user']
+        new_post.title = request.GET['title']
+        new_post.content = request.GET['content']
+        new_post.save()
+    return redirect()
