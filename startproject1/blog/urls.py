d9ac8495fb7f74d90bae2b2eba2bf4a76cf19f2b
sulmo <tjdah0853@gmail.com>
2019-08-02 17:54:05 +0900

    blog.views.post_new/post_edit 뷰를 구현

--- startproject1/blog/urls.py
+++ startproject1/blog/urls.py
@@ -6,5 +6,8 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('<int:id>/', views.post_detail, name='post_detail'),
-    path('create/', views.create, name="create"),
+    # path('create/', views.create, name="create"),
+    path('post/new/', views.post_new, name="post_new"),
+    path('post/edit/<int:id>', views.post_edit, name="post_edit"),
+
 ]
--- startproject1/blog/views.py
+++ startproject1/blog/views.py
@@ -1,5 +1,7 @@
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404, redirect
+
+from .forms import PostForm
 from .models import Post
 
 
@@ -19,16 +21,33 @@ def post_detail(request, id):
     #     post = Post.objects.get(id=id)
     # except Post.DoesNotExist:
     #     raise Http404
-    #post = Post.objects.get(id=id)
+    # post = Post.objects.get(id=id)
     post = get_object_or_404(Post, id=id)
     return render(request, 'blog/post_detail.html', {'post': post})
 
 
-def create(request):
-    if request.POST:
-        new_post = Post()
-        new_post.user = request.GET['user']
-        new_post.title = request.GET['title']
-        new_post.content = request.GET['content']
-        new_post.save()
-    return redirect()
+def post_new(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save()
+            return redirect('post_list')
+    else:
+        form = PostForm()
+    return render(request, 'blog/make_post.html', {
+        'form': form,
+    })
+
+
+def post_edit(request, id):
+    post= get_object_or_404(Post, id=id)
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            post = form.save()
+            return redirect('post_list')
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/make_post.html', {
+        'form': form,
+    })
